43c6e6b7a0d45069b42b2cf0c72647101d538efd
Hiba <hibakm72@gmail.com>
2025-03-20 10:30:51 +0530

    folder structure initialised (#9)

---
+++ src/components/auth/login/loginForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginForm() {
+  return <div>loginForm</div>;
+}
+
+export default LoginForm;
---
+++ src/components/auth/register/registerForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RegisterForm() {
+  return <div>registerForm</div>;
+}
+
+export default RegisterForm;
