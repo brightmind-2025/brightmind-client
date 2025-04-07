189d12fd3c68d972d2b0bfe26309a6321763ad09
hiba-0325 <hibakm72@gmail.com>
2025-04-07 19:48:38 +0530

    feat: implement authentication slice and thunks for user management

---
+++ src/lib/thunks/authThunks.ts
@@ -0,0 +1,97 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = "http://localhost:4008/api/user";
+
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (
+    userData: { name: string; email: string; password: string },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await axios.post(`${API_URL}/register`, userData);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        return rejectWithValue(err.response.data);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const activateUser = createAsyncThunk(
+  "auth/activateUser",
+  async (otpData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${API_URL}/activate-user`, otpData);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        return rejectWithValue(err.response.data);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${API_URL}/login-user`, credentials);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        return rejectWithValue(err.response.data);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const logoutUser = createAsyncThunk(
+  "auth/logoutUser",
+  async (_, { rejectWithValue }) => {
+    try {
+      await axios.get(`${API_URL}/logout-user`);
+      return true;
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        return rejectWithValue(err.response.data);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const refreshToken = createAsyncThunk(
+  "auth/refreshToken",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${API_URL}/refresh-token`);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        return rejectWithValue(err.response.data);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const loadUser = createAsyncThunk(
+  "auth/loadUser",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${API_URL}/user-info`);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        return rejectWithValue(err.response.data);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
