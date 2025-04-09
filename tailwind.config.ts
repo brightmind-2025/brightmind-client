c0614630cfd3e7957cf1b120da2b456a43ea0806
hiba-0325 <hibakm72@gmail.com>
2025-04-09 16:44:10 +0530

    feat: update configuration files for Next.js and Tailwind CSS; add PostCSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,17 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
   darkMode: "class",
+
   theme: {
     extend: {
-      fontFamily: {
-        Poppins: ["var(--font-Poppins)"],
-        Josefin: ["var(--font-Josefin)"],
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      screens: {
-        "400px": "400px",
-        "800px": "800px",
-        "1000px": "1000px",
-        "1100px": "1100px",
-        "1200px": "1200px",
-        "1300px": "1300px",
-        "1400px": "1400px",
-        "1500px": "1500px",
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
       },
     },
   },
   plugins: [],
 };
-
-export default config;
