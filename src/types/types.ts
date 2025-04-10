4ea700cdce8206cb4d95a21d327849d9e0bcd42f
hiba-0325 <hibakm72@gmail.com>
2025-04-10 14:33:16 +0530

    Refactor: Remove unused components and hooks

--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,36 @@ export interface UserDetails {
     refreshToken?: string; 
   }
   
+  export interface CourseData {
+    title: string;
+    description: string;
+    videoUrl: string;
+    videoThumbnail: string;
+    videoSection: string;
+    videoLength: number;
+    videoPlayer: string;
+    links: { title: string; url: string }[];
+    suggestion: string;
+    questions: []; 
+  }
+  
+  export interface Course {
+    _id: string;
+    name: string;
+    description: string;
+    price: number;
+    thumbnail: {
+      public_id: string;
+      url: string;
+    };
+    tags: string;
+    level: string;
+    demoUrl: string;
+    benefits: { title: string }[];
+    prerequisites: { title: string }[];
+    reviews: [];
+    courseData: CourseData[];
+    ratings?: number;
+    purchased?: number;
+  }
+  
